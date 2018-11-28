5ee0f15f0b0a0cd5803f9ed61f7084155b0f7bed
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-28 21:40:05 +0000

    Updated DockingStation dock(Bike) red phase

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,14 +3,17 @@ require 'bike'
 
 describe DockingStation do
 
-  bike = Bike.new
-
     it { is_expected.to respond_to :release_bike }
     it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to :bike }
 
   it 'releases bikes that work' do
     expect(subject.release_bike).to be_working
+  end
+
+  it 'is able to dock' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
   end
 
 end
