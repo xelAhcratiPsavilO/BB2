7d9ece2a8cb6a9447cf65713f103f17120c649ce
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-28 21:48:17 +0000

    Refactored docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,14 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
     @bike = bike
-  end
-
-  def bike
-    @bike
   end
 
 end
