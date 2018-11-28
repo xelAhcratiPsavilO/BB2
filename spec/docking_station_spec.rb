1460571e609353c6059737f651cc4a2274448b2d
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-28 20:38:27 +0000

    Expected DockerStation release_bike to output instance of Bike red phase

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
+require 'bike'
 describe DockingStation do
 
+  bike = Bike.new
+
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases bikes that work' do
+    expect(subject.release_bike).to be_working
+  end
 
 end
