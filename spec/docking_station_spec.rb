32b82826fe8c74d58e3ccf1ed7fc8a817e0b3596
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-28 21:47:09 +0000

    Refactored docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,21 +3,17 @@ require 'bike'
 
 describe DockingStation do
 
-    it { is_expected.to respond_to :release_bike }
-    it { is_expected.to respond_to(:dock).with(1).argument }
-    it { is_expected.to respond_to :bike }
+  bike = Bike.new
 
   it 'releases bikes that work' do
     expect(subject.release_bike).to be_working
   end
 
   it 'is able to dock' do
-    bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
 
   it 'is able to return docked bikes' do
-    bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
