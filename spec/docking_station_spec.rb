251f285279acc2a58d4cadd1940f5b4f9c2ca439
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-28 21:32:14 +0000

    Created DockingStation dock(Bike) red phase

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
+  bike = Bike.new
+
+    it { is_expected.to respond_to :release_bike }
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'releases bikes that work' do
     expect(subject.release_bike).to be_working
