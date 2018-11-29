e2c715ca1c2b35cb2639d620dd6ac99aac88f079
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-11-29 23:27:35 +0000

    Updated DockingStation release_bike red phase

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,18 +5,27 @@ describe DockingStation do
 
   bike = Bike.new
 
-  it 'releases bikes that work' do
-    subject.dock(bike)
-    expect(subject.release_bike).to be_working
+  describe '#release' do
+    it 'raises an error if docking station is empty' do
+      expect{ subject.release_bike }.to raise_error 'No bikes available'
+    end
+    it 'releases bikes that work' do
+      subject.dock(bike)
+      expect(subject.release_bike).to be_working
+    end
   end
 
-  it 'is able to dock' do
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock' do
+    it 'is able to dock' do
+      expect(subject.dock(bike)).to eq bike
+    end
   end
 
-  it 'is able to return docked bikes' do
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#bike' do
+    it 'is able to return docked bikes' do
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
   end
 
 end
