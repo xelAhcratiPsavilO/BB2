3b19e8bb401ea9792c8c154b427fe4083029fa0e
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-12-02 00:13:16 +0100

    Updated tests to include capacity refactor phase

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,23 @@ class DockingStation
   attr_reader :bikes
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking Station full' if @bikes.count >= 20
+    fail 'Docking Station full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
