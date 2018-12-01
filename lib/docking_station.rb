b779b9d5cff67377a91312b7f927c1f30bf7ef91
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-12-02 00:05:41 +0100

    Updated tests to include capacity green phase

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
+  def initialize
+    @bikes = []
+  end
+
+  attr_reader :bikes
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking Station full' if @bike
-    @bike = bike
+    fail 'Docking Station full' if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
