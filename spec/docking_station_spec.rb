64165fb9b1c33113dcc4d15d625e744b410715d8
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-08 21:39:20 +0000

    Isolated tests with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,10 @@ require 'bike'
 
 describe DockingStation do
 
-  bike = Bike.new
-  bike2 = Bike.new
-
   describe '#initialize' do
     it 'defaults capacity' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(bike) }
-      expect{ subject.dock(bike) }.to raise_error 'Docking Station full'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(double(:bike)) }
+      expect{ subject.dock(double(:bike)) }.to raise_error 'Docking Station full'
     end
   end
 
@@ -19,29 +16,29 @@ describe DockingStation do
     end
     it 'raises an error if bike is broken' do
       bike.report_broken
-      subject.dock(bike)
+      subject.dock(double(:bike))
       expect{ subject.release_bike }.to raise_error 'This bike is broken'
     end
     it 'releases bikes that work' do
-      subject.dock(bike2)
-      expect(subject.release_bike).to eq bike2
+      subject.dock(double(:bike2))
+      expect(subject.release_bike).to eq double(:bike2)
     end
   end
 
   describe '#dock' do
     it 'raises an error if docking station is full' do
-      subject.capacity.times { subject.dock(bike) }
-      expect{ subject.dock(bike) }.to raise_error 'Docking Station full'
+      subject.capacity.times { subject.dock(double(:bike)) }
+      expect{ subject.dock(double(:bike)) }.to raise_error 'Docking Station full'
     end
     it 'is able to dock' do
-      expect(subject.dock(bike)).to eq [bike]
+      expect(subject.dock(double(:bike))).to eq [double(:bike)]
     end
   end
 
   describe '#bikes' do
     it 'is able to return docked bikes' do
-      subject.dock(bike)
-      expect(subject.bikes).to eq [bike]
+      subject.dock(double(:bike))
+      expect(subject.bikes).to eq [double(:bike)]
     end
   end
 
