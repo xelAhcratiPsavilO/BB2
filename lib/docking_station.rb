700baf9d2b558e7b211a03b19f08a3e1ebc286dc
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-03-04 04:11:38 +0000

    Added guard condition to pick_from refactor phase

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -14,7 +14,7 @@ class DockingStation
 
   def release_bike(to = "user")
     fail 'No bikes available' if empty?
-      fail 'This bike is broken' if bike_broken? && to == "user"
+    fail 'This bike is broken' if bike_broken? && to == "user"
     @bikes.pop
   end
 
